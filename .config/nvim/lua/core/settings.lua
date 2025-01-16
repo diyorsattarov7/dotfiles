4cd107dc742e15f1030f7fc9c6b00117f1aea9dd
Diyor Sattarov <diyorsattarov@outlook.com>
2025-01-16 10:22:33 -0600

    feat: add core vim editor settings

---
+++ .config/nvim/lua/core/settings.lua
@@ -0,0 +1,15 @@
+-- core/settings.lua
+
+-- General settings
+vim.o.expandtab = true
+vim.o.shiftwidth = 2
+vim.o.tabstop = 4
+vim.o.termguicolors = true
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.signcolumn = 'yes'
+vim.o.cursorline = true
+
+-- Leader key
+vim.g.mapleader = ' ' -- Set leader key to space
+
