89b8fdaec630d7b30821d94e903e8e7c82f16b25
Diyor Sattarov <diyorsattarov@outlook.com>
2025-02-21 12:40:00 -0600

    chore: add clang-format

--- .config/nvim/lua/plugins/packer.lua
+++ .config/nvim/lua/plugins/packer.lua
@@ -2,58 +2,103 @@
 
 local packer = require('packer')
 packer.startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use 'tpope/vim-surround'
+    use 'wbthomason/packer.nvim'
+    use 'tpope/vim-surround'
 
-  -- Fuzzy finder
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = { 'nvim-lua/plenary.nvim' }
-  }
+    use {
+        'nvim-telescope/telescope.nvim',
+        requires = { 'nvim-lua/plenary.nvim' }
+    }
 
-  -- LSP setup
-  use {
-    'neovim/nvim-lspconfig',
-    'williamboman/mason.nvim',
-    'williamboman/mason-lspconfig.nvim',
-  }
+    use {
+        'neovim/nvim-lspconfig',
+        'williamboman/mason.nvim',
+        'williamboman/mason-lspconfig.nvim',
+    }
 
-  -- Completion
-  use {
-    'hrsh7th/nvim-cmp',
-    requires = {
-      'hrsh7th/cmp-nvim-lsp',
-      'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip',
-      'hrsh7th/cmp-path',
-      'hrsh7th/cmp-buffer',
-    },
-  }
+    use {
+        'hrsh7th/nvim-cmp',
+        requires = {
+            'hrsh7th/cmp-nvim-lsp',
+            'L3MON4D3/LuaSnip',
+            'saadparwaiz1/cmp_luasnip',
+            'hrsh7th/cmp-path',
+            'hrsh7th/cmp-buffer',
+        },
+    }
 
-  -- Git integration
-  use 'lewis6991/gitsigns.nvim'
+    use 'lewis6991/gitsigns.nvim'
 
-  -- Treesitter
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate'
-  }
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate'
+    }
 
-  -- Rust tools and debug support
-  use 'simrat39/rust-tools.nvim'
-  use 'mfussenegger/nvim-dap'
-  use 'nvim-lua/plenary.nvim'
+    use 'simrat39/rust-tools.nvim'
+    use 'mfussenegger/nvim-dap'
+    use 'nvim-lua/plenary.nvim'
 
-  use {
-      'iamcco/markdown-preview.nvim',
-      run = function() vim.fn['mkdp#util#install']() end,
-      ft = { 'markdown' },
-  }
+    use {
+        'iamcco/markdown-preview.nvim',
+        run = function() vim.fn['mkdp#util#install']() end,
+        ft = { 'markdown' },
+    }
+
+    use({
+        'projekt0n/github-nvim-theme',
+        config = function()
+            require('github-theme').setup({
+                options = {
+                    terminal_colors = true,
+                    styles = {
+                        comments = 'italic',
+                        keywords = 'bold',
+                        functions = 'bold',
+                    },
+                }
+            })
+        end
+    })
+
+    use({
+        'mhartington/formatter.nvim',
+        config = function()
+            require('formatter').setup({
+                filetype = {
+                    cpp = {
+                        function()
+                            return {
+                                exe = "clang-format",
+                                args = { "--assume-filename=.cpp" }, 
+                                stdin = true
+                            }
+                        end
+                    },
+                    hpp = {
+                        function()
+                            return {
+                                exe = "clang-format",
+                                args = { "--assume-filename=.hpp" }, 
+                                stdin = true
+                            }
+                        end
+                    }
+                }
+            })
+
+            vim.api.nvim_create_augroup("FormatAutogroup", { clear = true })
+            vim.api.nvim_create_autocmd("BufWritePost", {
+                group = "FormatAutogroup",
+                pattern = { "*.cpp", "*.hpp" },
+                command = "FormatWrite",
+            })
+        end
+    })
 
 
-  -- Sync packer if bootstrapping
-  if is_bootstrap then
-    require('packer').sync()
-  end
+
+    if is_bootstrap then
+        require('packer').sync()
+    end
 end)
 
