d570675a46bdadedaf56a0c929e4156c15ddf7ea
Diyor Sattarov <diyorsattarov@outlook.com>
2025-02-21 12:39:10 -0600

    chore: add objc to treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,10 @@
 -- plugins/treesitter.lua
 
 require('nvim-treesitter.configs').setup({
-  ensure_installed = { "lua", "python", "javascript", "typescript", "cpp", "c" },
+  ensure_installed = { 
+    "lua", "python", "javascript", "typescript", "cpp", "c", 
+    "markdown", "markdown_inline",  -- Markdown support
+    "objc", "cpp", "c",  -- Objective-C support (Treesitter uses "objc" for Objective-C)
+  },
   highlight = { enable = true },
 })
-
