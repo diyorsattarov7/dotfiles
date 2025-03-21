7c55bb572a985f30a144a3db17552e74af3205a1
Diyor Sattarov <diyorsattarov@outlook.com>
2025-03-20 19:25:58 -0500

    fix: vimtex zathura integration for PDF viewing

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -1,9 +1,7 @@
 -- plugins/vimtex.lua
 local g = vim.g
 
-g.vimtex_view_method = 'zathura' 
 g.vimtex_view_general_viewer = 'zathura'
-g.vimtex_view_general_options = '--unique file:@pdf\\#src:@line@tex'
 
 g.vimtex_compiler_method = 'latexmk'
 g.vimtex_compiler_latexmk = {
