7a2a2416bce98992aa39ba2fd330cd8bfb329974
Diyor Sattarov <diyorsattarov@outlook.com>
2025-04-15 13:40:51 -0700

    feat: Initial neovim configuration

---
+++ .config/nvim/lua/core/keymaps.lua
@@ -0,0 +1,6 @@
+-- core/keymaps.lua
+
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+vim.keymap.set('n', '<leader>x', "\"+y")
+vim.keymap.set('v', '<leader>x', "\"+y")
+vim.keymap.set('n', '<leader>d', 'x')
