88f17148204a464c118de000ed47c082c1ecce5c
Diyor Sattarov <diyorsattarov@outlook.com>
2025-04-15 13:40:51 -0700

    feat: Initial neovim configuration

---
+++ .config/nvim/lua/core/settings.lua
@@ -0,0 +1,11 @@
+-- core/settings.lua
+
+vim.o.expandtab = true
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.termguicolors = true
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.signcolumn = 'yes'
+vim.o.cursorline = true
+vim.g.mapleader = ' '
