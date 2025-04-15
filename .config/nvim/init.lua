9b09b9d95f9fa8ab13d5846bad6ac6aa3db625a4
Diyor Sattarov <diyorsattarov@outlook.com>
2025-04-15 15:21:45 -0700

    feat: Add bootstrap and telescope keymaps

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,6 @@
+-- init.lua
+local bootstrap = require('bootstrap')
+local is_bootstrap = bootstrap.ensure_packer()
+
 require('core.settings')
-require('core.keymaps')
+require('core.keymaps').setup()
--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -1,6 +1,25 @@
 -- core/keymaps.lua
 
-vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
-vim.keymap.set('n', '<leader>x', "\"+y")
-vim.keymap.set('v', '<leader>x', "\"+y")
-vim.keymap.set('n', '<leader>d', 'x')
+local M = {}
+
+M.setup_basic = function()
+    vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+    vim.keymap.set("n", "<leader>x", "\"+y")
+    vim.keymap.set("v", "<leader>x", "\"+y")
+    vim.keymap.set("n", "<leader>d", "x")
+end
+
+M.setup_telescope = function()
+    local ok, builtin = pcall(require, 'telescope.builtin')
+    if not ok then
+        return
+    end
+    vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+
+end
+
+function M.setup()
+    M.setup_basic()
+    M.setup_telescope()
+end
+return M
