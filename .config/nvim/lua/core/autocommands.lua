78602a948dccabb514e19fdd8965653b7b18587e
Diyor Sattarov <diyorsattarov@outlook.com>
2025-07-23 18:44:28 -0500

    feat: add highlight on yank

--- .config/nvim/lua/core/autocommands.lua
+++ .config/nvim/lua/core/autocommands.lua
@@ -1,5 +1,11 @@
 -- plugins/autocommands.lua
 
+vim.api.nvim_create_autocmd("TextYankPost", {
+    callback = function()
+        vim.highlight.on_yank { timeout = 200 }
+    end,
+})
+
 vim.api.nvim_create_autocmd("BufReadPost", {
     pattern = "*",
     callback = function()
